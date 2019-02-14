c5e8ad92a21b6e8161a2afd54bf972745466c33f
ethan <ethan@ArchAngel>
2019-02-14 15:41:12 +0930

    Setting up frame

--- FooBall/src/FooBall_App.java
+++ FooBall/src/FooBall_App.java
@@ -2,9 +2,14 @@
 public class FooBall_App {
 
 	
+	//Set parameters of program start
+	int[] dimensions = {750, 750};
+	
 	//Main Method to start program
 	public static void main(String[] args) {
 
+		//Create Frame/Window
+		FooBall_Frame frame = new FooBall_Frame();
 		
 		
 	}
--- FooBall/src/FooBall_Frame.java
+++ FooBall/src/FooBall_Frame.java
@@ -3,6 +3,13 @@ import javax.swing.JFrame;
 //Extend JFrame to make the window it's own object
 public class FooBall_Frame extends JFrame{
 
+	//Create variables for the frame
+	int[] dimensions;
+	
+	public void init(int[] dimensions) {
+		//Set dimensions for the frame as those passed by main method
+		this.dimensions = dimensions;
+	}
 	
 	
 }
