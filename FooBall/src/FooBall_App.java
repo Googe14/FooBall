0a7a4d22f1fc57e25110788b3bc5544557267c84
ethan <ethan@UbuntuBeast>
2019-02-16 11:45:31 +0930

    Finalizing Main Method

--- FooBall/src/FooBall_App.java
+++ FooBall/src/FooBall_App.java
@@ -1,17 +1,27 @@
 
 public class FooBall_App {
-
-	
-	//Set parameters of program start
-	int[] dimensions = {750, 750};
 	
 	//Main Method to start program
 	public static void main(String[] args) {
 
+		
+		//Set a few universal parameters - Being set in main method to easily modify at any time
+		//Create dimensions array which will hold the dimensions of the Gamepanel, not the whole frame
+		int[] dimensions = {750, 750};
+		String name = "FooBall";
+		
+		
+		
 		//Create Frame/Window
 		FooBall_Frame frame = new FooBall_Frame();
+		//Initialize frame and create panels o go on it
+		frame.init(dimensions, name);
+		frame.genPanels();
+		//Start frame and continue from there
+		frame.start();
 		
 		
+		//**End of Main Method - Continue from FooBall_Frame.java - void start()**//
 	}
 
 }
